9d5ba5ec245b3e1955e776ce82744a097e9208da
Dwi Han <dihantechid@gmail.com>
2022-05-12 13:54:06 +0700

    validation

--- app/Models/Camp.php
+++ app/Models/Camp.php
@@ -2,6 +2,8 @@
 
 namespace App\Models;
 
+use App\Models\Checkout;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
@@ -13,4 +15,13 @@ class Camp extends Model
     protected $fillable = [
         'title', 'price'
     ];
+
+    public function getIsRegisteredAttribute()
+    {
+        if (!Auth::check()) {
+            return false;
+        }
+
+        return Checkout::whereCampId($this->id)->whereUserId(Auth::id())->exists();
+    }
 }
